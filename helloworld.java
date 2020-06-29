fa1efa12f1490bb26deac6b96514225e30b93e9c
anluping <2540113979@qq.com>
2020-06-29 17:18:34 +0800

    哈咯

---
+++ helloworld.java
@@ -0,0 +1,27 @@
+//public表示公开的
+//class表示定义一个类
+//hello world表示一个类名
+public class helloworld //表示定义一个公开的类，类名hello world
+{
+	//类体中不允许直接编写Java语句【出声明变量外】
+	//类体{记住}
+	/*
+	static表示静态的
+	void表示空
+	main表示方法名是main
+	（String[] args）是一个main方法的形式参数列表
+
+	需要注意的是：以下方法是一个程序的主方法，是程序的执行入口
+	*/
+	public static void main(String[] args) //表示定义一个公开的静态的主方法
+	{
+		//Java语句以“；”终止，分号必须是半角分号
+		//向控制台输出一段消息，半角双引号
+		//System.out.println("Hello World!");
+		//输出中文
+		System.out.println("helloworld");
+		//System.out.println("nmhao");
+		//System.out.println("你好!");
+		
+	}
+}
